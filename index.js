e0dabdee67ca59d82905b97544f7991a34d4838e
Robinson Junior <hardzork@macjr.local>
2019-06-27 09:28:02 -0300

    Challenge completed.

--- index.js
+++ index.js
@@ -7,11 +7,40 @@ server.use(express.json());
 //arrProjects é o lugar onde serão armazenados os projetos e suas tarefas
 const arrProjects = [];
 
-/*
-ROTA: POST /projects: A rota deve receber ide title dentro do corpo de cadastrar um 
-novo projeto dentro de um array no seguinte formato { id: "1", title: "Novo Projeto", tasks: [] }; 
-Certifique-se de enviar tanto o ID quanto o título do projeto no formato string com àspas duplas.
-*/
+//requestCounter será o lugar onde armazenaremos a quantidade de requisições feitas em nessa api.
+let requestCounter = 0;
+
+/**
+ * Crie um middleware global chamado em todas requisições que imprime (console.log)
+ * uma contagem de quantas requisições foram feitas na aplicação até então;
+ */
+server.use((req, res, next) => {
+  requestCounter++;
+  console.log(`Requisições feitas até o momento: ${requestCounter}`);
+  return next();
+});
+
+/**
+ * Crie um middleware que será utilizado em todas rotas que recebem o ID do projeto
+ * nos parâmetros da URL que verifica se o projeto com aquele ID existe.
+ * Se não existir retorne um erro, caso contrário permita a requisição continuar normalmente;
+ */
+function verifyProjectExistence(req, res, next) {
+  const { id } = req.params;
+  const found = arrProjects.find(item => item.id === id);
+
+  if (!found) {
+    return res.status(400).json({ message: "Project not found!" });
+  }
+
+  return next();
+}
+
+/**
+ * ROTA: POST /projects: A rota deve receber ide title dentro do corpo de cadastrar um
+ * novo projeto dentro de um array no seguinte formato { id: "1", title: "Novo Projeto", tasks: [] };
+ * Certifique-se de enviar tanto o ID quanto o título do projeto no formato string com àspas duplas.
+ */
 server.post("/projects", (req, res) => {
   const { id, title } = req.body;
   const newProject = {
@@ -33,7 +62,7 @@ server.get("/projects", (req, res) => {
 /*
 ROTA: PUT /projects/:id: A rota deve alterar apenas o título do projeto com o id presente nos parâmetros da rota;
 */
-server.put("/projects/:id", (req, res) => {
+server.put("/projects/:id", verifyProjectExistence, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -46,7 +75,7 @@ server.put("/projects/:id", (req, res) => {
 /*
 ROTA: DELETE /projects/:id: A rota deve deletar o projeto com o id presente nos parâmetros da rota;
 */
-server.delete("/projects/:id", (req, res) => {
+server.delete("/projects/:id", verifyProjectExistence, (req, res) => {
   const { id } = req.params;
 
   const index = arrProjects.findIndex(item => item.id === id);
@@ -60,7 +89,7 @@ ROTA: POST /projects/:id/tasks: A rota deve receber um campo title e armazenar
 uma nova tarefa no array de tarefas de um projeto específico escolhido 
 através do id presente nos parâmetros da rota;
 */
-server.post("/projects/:id/tasks", (req, res) => {
+server.post("/projects/:id/tasks", verifyProjectExistence, (req, res) => {
   const { id } = req.params;
   const { taskTitle } = req.body;
 
