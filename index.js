fecf007afaca60186dedbec423558e6236a8e545
Robinson Junior <hardzork@macjr.local>
2019-06-26 22:56:08 -0300

    routes and insomnia tests.

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ novo projeto dentro de um array no seguinte formato { id: "1", title: "Novo Proj
 Certifique-se de enviar tanto o ID quanto o título do projeto no formato string com àspas duplas.
 */
 server.post("/projects", (req, res) => {
-  let { id, title } = req.body;
-  let newProject = {
+  const { id, title } = req.body;
+  const newProject = {
     id,
     title,
     tasks: []
@@ -30,6 +30,9 @@ server.get("/projects", (req, res) => {
   return res.json(arrProjects);
 });
 
+/*
+ROTA: PUT /projects/:id: A rota deve alterar apenas o título do projeto com o id presente nos parâmetros da rota;
+*/
 server.put("/projects/:id", (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -40,56 +43,33 @@ server.put("/projects/:id", (req, res) => {
   return res.status(201).json({ message: "Project updated successfuly!" });
 });
 
-// server.use((req, res, next) => {
-//   console.time("Request");
-//   console.log(`método: ${req.method}; URL: ${req.url}`);
-//   next();
-//   console.timeEnd("Request");
-// });
+/*
+ROTA: DELETE /projects/:id: A rota deve deletar o projeto com o id presente nos parâmetros da rota;
+*/
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
 
-// function checkUserExists(req, res, next) {
-//   if (!req.body.name) {
-//     return res.status(400).json({ error: "User name is required" });
-//   }
-//   return next();
-// }
+  const index = arrProjects.findIndex(item => item.id === id);
+  arrProjects.splice(index, 1);
 
-// function checkUserInArray(req, res, next) {
-//   const user = users[req.params.index];
-//   if (!user) {
-//     return res.status(400).json({ error: "User does not exists" });
-//   }
-//   req.user = user;
-//   return next();
-// }
+  return res.status(200).json({ message: "Project removed successfuly!" });
+});
 
-// server.get("/users", (req, res) => {
-//   return res.json(users);
-// });
+/*
+ROTA: POST /projects/:id/tasks: A rota deve receber um campo title e armazenar 
+uma nova tarefa no array de tarefas de um projeto específico escolhido 
+através do id presente nos parâmetros da rota;
+*/
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { taskTitle } = req.body;
 
-// server.get("/users/:index", checkUserInArray, (req, res) => {
-//   const { index } = req.params;
+  const found = arrProjects.find(item => item.id === id);
+  found.tasks.push(taskTitle);
 
-//   return res.json(req.user);
-// });
-
-// server.post("/users", checkUserExists, (req, res) => {
-//   const { name } = req.body;
-//   users.push(name);
-//   return res.json(users);
-// });
-
-// server.put("/users/:index", checkUserInArray, checkUserExists, (req, res) => {
-//   const { index } = req.params;
-//   const { name } = req.body;
-//   users[index] = name;
-//   return res.json(users);
-// });
-
-// server.delete("/users/:index", checkUserInArray, (req, res) => {
-//   const { index } = req.params;
-//   users.splice(index, 1);
-//   res.send();
-// });
+  return res
+    .status(200)
+    .json({ message: `Task created on ${found.title} Project!` });
+});
 
 server.listen(3000);
